579fb738fe74adbea6e1df8b26c5e44f5f91cbd1
Mushfequr Rahman <mushfeq.rahman@gmail.com>
2017-07-31 11:32:07 -0700

    added --prune-empty-dirs

--- sync.sh
+++ sync.sh
@@ -1,1 +1,2 @@
-rsync -auh --delete --quiet --stats --progress /home/username007/project1 /media/sf_Ubuntu_shared/
+#man page https://linux.die.net/man/1/rsync
+rsync -auh --prune-empty-dirs --delete --quiet --stats --progress /home/username007/project1 /media/sf_Ubuntu_shared/
